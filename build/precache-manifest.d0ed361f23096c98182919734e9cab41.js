b6e4e3fb1648d897c0fa15f0046a7180c058d688
daifang <784433957@qq.com>
2019-10-17 22:01:02 +0800

    2019-10-17

---
+++ build/precache-manifest.d0ed361f23096c98182919734e9cab41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895cdcc49c6dd8ea3bc983ef752c2dd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a837501617355df190",
+    "url": "/static/css/main.6269cf2e.chunk.css"
+  },
+  {
+    "revision": "cab23a528abbb1b7d800",
+    "url": "/static/js/2.4ec661e5.chunk.js"
+  },
+  {
+    "revision": "83a837501617355df190",
+    "url": "/static/js/main.cddec664.chunk.js"
+  },
+  {
+    "revision": "2676ba806d60bc0d5ff1",
+    "url": "/static/js/runtime-main.7dcd6b0b.js"
+  }
+]);
